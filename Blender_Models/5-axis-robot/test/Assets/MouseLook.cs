f2d66b63039bea620eb5178db2981858a890e5a7
piotex <pkubon2@gmail.com>
2021-11-11 16:49:50 +0100

    Added dynamic changing view

--- Blender_Models/5-axis-robot/test/Assets/MouseLook.cs
+++ Blender_Models/5-axis-robot/test/Assets/MouseLook.cs
@@ -4,25 +4,39 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    public bool DynamicView;
     public float mouseSensitivity = 300f;
     private float xRotation = 0f;
     public Transform playerBody;
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;                                                           //ukrywanie kursora
+        DynamicView = false;
+        //Cursor.lockState = CursorLockMode.Locked;                                                           //ukrywanie kursora
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        if (DynamicView)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+    }
+    public void ChangeDynamicView()
+    {
+        DynamicView = !DynamicView;
+        if (!DynamicView)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0f, 0f);
+            playerBody.rotation = Quaternion.Euler(0,0,0);
+        }
     }
 }
